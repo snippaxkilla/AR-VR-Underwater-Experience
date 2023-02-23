e7cdd860a50b1307d1c3d89866c86be6006b991a
snippaxkilla <43402335+snippaxkilla@users.noreply.github.com>
2023-02-23 21:46:09 +0100

    updated rays to fire two seperate rays
    and fixed the visual rays to also be seperate, this will also provide two aiming reticles

--- Assets/Scripts/RaycastPlaceObject.cs
+++ Assets/Scripts/RaycastPlaceObject.cs
@@ -1,7 +1,5 @@
 using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using UnityEditor;
 using UnityEngine;
 
 public class RaycastPlaceObject : MonoBehaviour
@@ -10,99 +8,101 @@ public class RaycastPlaceObject : MonoBehaviour
     // but we set it on 0.01f to avoid the object to be clipped in the ground
     [SerializeField] private bool rayVisible = true;
     [SerializeField] private int maxObjects = 1;
-    [SerializeField] private float offsetX;
-    [SerializeField] private float offsetY = 0.01f;
-    [SerializeField] private float offsetZ;
+    [SerializeField] private Vector3 offset;
+    [SerializeField] private OVRInput.RawButton[] leftButtons;
+    [SerializeField] private OVRInput.RawButton[] rightButtons;
 
     public GameObject objectToPlace;
-    public Transform _targetingIcon;
-    public LineRenderer _raycastLine;
+    public Transform _targetingIconLeft;
+    public Transform _targetingIconRight;
+    public LineRenderer _raycastLineLeft;
+    public LineRenderer _raycastLineRight;
     public LayerMask _sceneLayer;
 
     Queue<GameObject> placedObjects = new();
 
-    void Start()
-    {
-        _raycastLine.enabled = false;
-    }
-    
     void Update()
     {
         OffsetCalculation();
-        Raycast();
+        UpdateRaycastLine();
+        var resultLeft = Raycast(OVRInput.Controller.LTouch, leftButtons);
+        var resultRight = Raycast(OVRInput.Controller.RTouch, rightButtons);
+        _targetingIconLeft.position = resultLeft.position;
+        _targetingIconRight.position = resultRight.position;
+        _targetingIconLeft.localScale = resultLeft.scale;
+        _targetingIconRight.localScale = resultRight.scale;
         //EditorTester();
     }
 
     void OffsetCalculation()
     {
-        if ((offsetX == 0) && (offsetY == 0) && (offsetZ == 0))
+        if (offset == Vector3.zero)
         {
             return;
         }
-        else
-        {
-            Vector3 objectPos = objectToPlace.transform.position;
-            offsetX += objectPos.x; offsetY += objectPos.y; offsetZ += objectPos.z;
-        }
+        var objectPos = objectToPlace.transform.position;
+        objectPos += offset;
+        objectToPlace.transform.position = objectPos;
     }
 
     // does the exact same as the raycast function, but it is to test it out on the computer
-    void EditorTester()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            placedObjects.Enqueue(Instantiate(objectToPlace, _targetingIcon.position, Quaternion.identity));
-            if (maxObjects > 0 && placedObjects.Count > maxObjects)
-            {
-                Destroy(placedObjects.Dequeue());
-            }
-        }
-    }
+    //void EditorTester()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //    {
+    //        placedObjects.Enqueue(Instantiate(objectToPlace, _targetingIcon.position, Quaternion.identity));
+    //        if (maxObjects > 0 && placedObjects.Count > maxObjects)
+    //        {
+    //            Destroy(placedObjects.Dequeue());
+    //        }
+    //    }
+    //}
 
-    void Raycast()
+    RaycastResult Raycast(OVRInput.Controller controller, OVRInput.RawButton[] buttons)
     {
-        RaycastHit hitInfo;
-        Vector3 rayPos = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
-        Vector3 rayFwd = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTouch) * Vector3.forward;
-        if (Physics.Raycast(rayPos, rayFwd, out hitInfo, 1000.0f, _sceneLayer))
+        var returnValue = new RaycastResult();
+        Vector3 rayPos = OVRInput.GetLocalControllerPosition(controller);
+        Vector3 rayFwd = OVRInput.GetLocalControllerRotation(controller) * Vector3.forward;
+        if (Physics.Raycast(rayPos, rayFwd, out var hitInfo, 1000.0f, _sceneLayer))
         {
             // if hitting a vertical surface, drop quad to the floor
             float iconHeight = Mathf.Abs(Vector3.Dot(Vector3.up, hitInfo.normal)) < 0.5f ? 0 : hitInfo.point.y;
             // offset quad a bit so it doesn't z-flicker
-            _targetingIcon.position = new Vector3(hitInfo.point.x, iconHeight + 0.01f, hitInfo.point.z);
+            returnValue.position = new Vector3(hitInfo.point.x, iconHeight + 0.01f, hitInfo.point.z);
         }
-        bool pressingButton = OVRInput.Get(OVRInput.RawButton.RIndexTrigger) || OVRInput.Get(OVRInput.RawButton.A);
-        Vector3 position = hitInfo.point + new Vector3(offsetX, offsetY, offsetZ);
+        bool pressingButton = buttons.Any(b => OVRInput.Get(b, controller));
+        Vector3 position = hitInfo.point + offset;
         if (pressingButton)
         {
-            placedObjects.Enqueue(Instantiate(objectToPlace, _targetingIcon.position, Quaternion.identity));
+            placedObjects.Enqueue(Instantiate(objectToPlace, position, Quaternion.identity));
             if (maxObjects > 0 && placedObjects.Count > maxObjects)
             {
                 Destroy(placedObjects.Dequeue());
             }
-            _targetingIcon.localScale = Vector3.one * (pressingButton ? 0.6f : 0.5f);
+            returnValue.scale = Vector3.one * 0.6f;
         }
+        return returnValue;
     }
 
-    void UpdateRaycastLine(Vector3 origin, Vector3 direction)
+    void UpdateRaycastLine()
     {
-        if (rayVisible)
-        {
-            _raycastLine.enabled = true;
-            _raycastLine.SetPosition(0, origin);
-            _raycastLine.SetPosition(1, _targetingIcon.position);
-        }
-        else
-        {
-            _raycastLine.enabled = false;
-        }
+        _raycastLineLeft.SetPosition(0, OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch));
+        _raycastLineRight.SetPosition(0, OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch));
+        _raycastLineLeft.SetPosition(1, _targetingIconLeft.position);
+        _raycastLineRight.SetPosition(1, _targetingIconRight.position);
     }
     
-    // debug function not used anywhere now
     void ToggleRayVisibility()
     {
         rayVisible = !rayVisible;
-        UpdateRaycastLine(Vector3.zero, Vector3.zero);
+        _raycastLineLeft.enabled = rayVisible;
+        _raycastLineRight.enabled = rayVisible;
+    }
+
+    struct RaycastResult
+    {
+        public Vector3 position;
+        public Vector3 scale;
     }
 }
 
