77da13cedf689a2a5d11e31a9a38746e1b3cc004
snippaxkilla <43402335+snippaxkilla@users.noreply.github.com>
2023-03-01 13:16:31 +0100

    layerchanger for portal v1

---
+++ Assets/Scripts/ChangeLayerPortal.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class ChangeLayerPortal : MonoBehaviour
+{
+    [Header("Put in the layer you want to change to in new layer vice versa")]
+    [SerializeField] private string newLayerName = "default";
+    [SerializeField] private string oldLayerName = "insidePortal";
+    [Header("Change children in this object to corresponding new layer")]
+    [SerializeField] private GameObject targetObject; // The GameObject whose children layers need to be changed
+
+    private Vector3 colliderNormal = Vector3.forward; // The normal direction of the collider surface
+
+    private int newLayer; // The integer value of the new layer
+    private int oldLayer; // The integer value of the old layer
+
+    private void Start()
+    {
+        newLayer = LayerMask.NameToLayer(newLayerName);
+        oldLayer = LayerMask.NameToLayer(oldLayerName);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Player")) return;
+        var direction = other.transform.position - transform.position;
+        if (Vector3.Dot(direction.normalized, colliderNormal) > 0)
+        {
+            ChangeChildrenLayer(targetObject.transform, newLayer);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Player")) return;
+        var direction = other.transform.position - transform.position;
+        if (Vector3.Dot(direction.normalized, colliderNormal) < 0)
+        {
+            ChangeChildrenLayer(targetObject.transform, oldLayer);
+        }
+    }
+
+    private static void ChangeChildrenLayer(Transform parent, int newLayer)
+    {
+        parent.gameObject.layer = newLayer;
+        foreach (Transform child in parent)
+        {
+            ChangeChildrenLayer(child, newLayer);
+        }
+    }
+}
