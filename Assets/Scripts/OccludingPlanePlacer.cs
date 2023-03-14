fed6f41ce006f1b0caa3490876ee48e9ee90ecee
snippaxkilla <43402335+snippaxkilla@users.noreply.github.com>
2023-03-14 11:07:26 +0100

    masking plane under portal
    plane will mask the portal from rising up and will destroy itself after the object is finished rising keep it longer for 0.1f in case of shake effect moving the portal down.

---
+++ Assets/Scripts/OccludingPlanePlacer.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class OccludingPlanePlacer : MonoBehaviour
+{
+    [SerializeField] private GameObject occludingPlane;
+
+    [Header("Object riser reference")]
+    [SerializeField] private ObjectRiser objectRiser;
+
+    private GameObject occludingPlaneInstance; // Keep a reference to the instantiated object
+
+    private void Start()
+    {
+        var spawnPosition = new Vector3(transform.position.x, 0, transform.position.z);
+        occludingPlaneInstance = Instantiate(occludingPlane, spawnPosition, Quaternion.identity); // Store reference to instantiated object
+    }
+
+    private void Update()
+    {
+        if (objectRiser.isRising) return;
+        // If the object is not rising, destroy the previously instantiated object
+        if (occludingPlaneInstance != null)
+        {
+            Destroy(occludingPlaneInstance, 0.1f);
+        }
+    }
+}
