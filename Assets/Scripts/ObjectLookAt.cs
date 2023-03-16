ffbd350afb34465fae672e628863095281b027bc
snippaxkilla <43402335+snippaxkilla@users.noreply.github.com>
2023-03-16 10:45:51 +0100

    fixed lookat function
    no more weird angles only rotate on y axis and is smoother

--- Assets/Scripts/ObjectLookAt.cs
+++ Assets/Scripts/ObjectLookAt.cs
@@ -4,13 +4,17 @@ public class ObjectLookAt : MonoBehaviour
 {
     [SerializeField] private GameObject target;
 
-    Vector3 targetPosition;
-
     private void Start()
     {
-        targetPosition = target.transform.position;
-        targetPosition.y = 0;
+        // Set the target's position to be at the same height as the current object
+        var targetPosition = target.transform.position;
+        targetPosition.y = transform.position.y;
 
-        transform.LookAt(targetPosition);
+        // Calculate the direction to the target without taking into account the height difference
+        var direction = targetPosition - transform.position;
+        direction.y = 0;
+
+        // Rotate the object around its y-axis towards the target
+        transform.rotation = Quaternion.LookRotation(direction);
     }
 }
