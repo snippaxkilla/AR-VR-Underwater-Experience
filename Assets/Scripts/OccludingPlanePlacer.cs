1fc855aeb3de337c194f571ce2b9f65e6f78217e
snippaxkilla <43402335+snippaxkilla@users.noreply.github.com>
2023-03-21 10:48:26 +0100

    turned off spawning out of ground

--- Assets/Scripts/OccludingPlanePlacer.cs
+++ Assets/Scripts/OccludingPlanePlacer.cs
@@ -7,23 +7,34 @@ public class OccludingPlanePlacer : MonoBehaviour
     [Header("Object riser reference")]
     [SerializeField] private ObjectRiser objectRiser;
 
-    private GameObject occludingPlaneInstance; // Keep a reference to the instantiated object
+    private GameObject occludingPlaneInstance;
 
     private void Start()
     {
         // Prevent z flicker by placing the plane slightly below the ground
         var spawnPosition = new Vector3(transform.position.x, -0.1f, transform.position.z);
-        occludingPlaneInstance = Instantiate(occludingPlane, spawnPosition, Quaternion.identity); // Store reference to instantiated object
+        occludingPlaneInstance = Instantiate(occludingPlane, spawnPosition, Quaternion.identity);
     }
 
     private void Update()
     {
+        DestroySmokeEarly();
         if (objectRiser.isRising) return;
         // If the object is not rising, destroy the previously instantiated object
         if (occludingPlaneInstance != null)
         {
-            // delay in accordance to the particle system duration
+            // Delay in accordance to the particle system duration
             Destroy(occludingPlaneInstance, 5f);
+        }
+    }
+
+    private void DestroySmokeEarly()
+    {
+        // Only destroy when a new object is spawned since the Oculus can't handle all the particles
+        if (occludingPlaneInstance == null) return;
+        for (var i = 0; i < occludingPlaneInstance.transform.childCount; i++)
+        {
+            Destroy(occludingPlaneInstance.transform.GetChild(i).gameObject);
         }
     }
 }
