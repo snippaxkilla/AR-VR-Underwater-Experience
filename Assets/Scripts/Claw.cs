832160be1fc84266ebc598a354f1ba840d77b06a
snippaxkilla <43402335+snippaxkilla@users.noreply.github.com>
2023-03-31 10:34:15 +0200

    grapplinghook is almost done
    no more errors but haven't tested it out yet.

--- Assets/Scripts/Claw.cs
+++ Assets/Scripts/Claw.cs
@@ -5,38 +5,23 @@ public class Claw : MonoBehaviour
     [SerializeField] private GameObject GroupedGarbage;
     [SerializeField] private GameObject Player;
     [SerializeField] private GameObject GrapplingHook;
-    [SerializeField] private float MaxDistance = 10f;
-    [SerializeField] private float PullSpeed = 5f;
 
-    private bool isPullingBack = false;
+    private bool isHooked = false;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.transform.parent == GroupedGarbage.transform)
         {
-            StartPullback();
+            StartPullback(other.gameObject);
         }
     }
 
-    private void StartPullback()
+    private void StartPullback(GameObject objectToPull)
     {
-        isPullingBack = true;
-    }
+        // Retrieve the GrapplingHook component and retract the hook
+        var grapplingHook = GrapplingHook.GetComponent<GrapplingHook>();
+        grapplingHook.RetractHook(gameObject, transform.position);
 
-
-    private void Update()
-    {
-        if (isPullingBack)
-        {
-            Vector3 direction = GrapplingHook.transform.position - GroupedGarbage.transform.position;
-
-            if (direction.magnitude > MaxDistance)
-            {
-                direction.Normalize();
-                direction *= PullSpeed * Time.deltaTime;
-
-                GroupedGarbage.transform.position += direction;
-            }
-        }
+        Destroy(objectToPull);
     }
 }
--- Assets/Scripts/GrapplingHook.cs
+++ Assets/Scripts/GrapplingHook.cs
@@ -1,51 +1,90 @@
+using System.Collections;
 using System.Linq;
 using UnityEngine;
 
 public class GrapplingHook : MonoBehaviour
 {
-    [SerializeField] private GameObject claw;
-    [SerializeField] private float MaxDistance = 10f;
-    [SerializeField] private float PullSpeed = 5f;
+    [SerializeField] private GameObject clawLeft;
+    [SerializeField] private GameObject clawRight;
+    [SerializeField] private float maxDistance = 10f;
+    [SerializeField] private float pullSpeed = 5f;
+    [SerializeField] private float forceMagnitude = 15f;
 
     [Header("Specify the buttons we want to use to shoot out hooks")]
     [SerializeField] private OVRInput.RawButton[] leftButtons;
     [SerializeField] private OVRInput.RawButton[] rightButtons;
 
-    private float currentDistance = 0;
+    private Vector3 clawLeftInitialPosition;
+    private Vector3 clawRightInitialPosition;
+    private float clawLeftCurrentDistance;
+    private float clawRightCurrentDistance;
+    private bool isLeftRetracting = false;
+    private bool isRightRetracting = false;
+    private float leftLerpStartTime;
+    private float rightLerpStartTime;
 
     private void Start()
     {
-
+        clawLeftInitialPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.LTouch);
+        clawRightInitialPosition = OVRInput.GetLocalControllerPosition(OVRInput.Controller.RTouch);
     }
 
     private void Update()
     {
-        
+        DistanceChecker();
+        ShootHook(OVRInput.Controller.LTouch, leftButtons, clawLeft, ref isLeftRetracting, ref leftLerpStartTime,
+            clawLeftInitialPosition, ref clawLeftCurrentDistance);
+        ShootHook(OVRInput.Controller.RTouch, rightButtons, clawRight, ref isRightRetracting, ref rightLerpStartTime,
+            clawRightInitialPosition, ref clawRightCurrentDistance);
     }
 
-    private void ShootHook(OVRInput.Controller controller, OVRInput.RawButton[] buttons)
+    private void ShootHook(OVRInput.Controller controller, OVRInput.RawButton[] buttons, GameObject claw,
+        ref bool isRetracting, ref float lerpStartTime, Vector3 clawInitialPosition, ref float clawCurrentDistance)
     {
-        Rigidbody claw = transform.GetChild(0).GetComponent<Rigidbody>();
+        var clawRigidbody = claw.GetComponent<Rigidbody>();
 
-        var rayPos = OVRInput.GetLocalControllerPosition(controller);
         var rayFwd = OVRInput.GetLocalControllerRotation(controller) * Vector3.forward;
 
         var pressingButton = buttons.Any(button => OVRInput.GetDown(button, controller));
 
-        var forceDirection = new Vector3(0f, 0f, 1f);
-        var forceMagnitude = 10f;
-        claw.AddForce(forceDirection * forceMagnitude, ForceMode.Impulse);
+        if (!pressingButton) return;
+        claw.transform.position = clawInitialPosition;
+        clawRigidbody.AddForce(rayFwd * forceMagnitude, ForceMode.Impulse);
+
+        isRetracting = false;
+        clawCurrentDistance = Vector3.Distance(transform.position, claw.transform.position);
     }
 
-    private void RetractHook()
+    public IEnumerator RetractHook(GameObject claw, Vector3 clawInitialPosition)
     {
+        var isRetracting = true;
+        var lerpStartTime = Time.time;
 
+        while (isRetracting)
+        {
+            var lerpFactor = (Time.time - lerpStartTime) / pullSpeed;
+            lerpFactor = Mathf.Clamp01(lerpFactor);
+            claw.transform.position = Vector3.Lerp(claw.transform.position, clawInitialPosition, lerpFactor);
+
+            if (lerpFactor >= 1)
+            {
+                isRetracting = false;
+            }
+
+            yield return null;
+        }
     }
 
-    // Retract the hook on max distance if no target has been pulled
     private void DistanceChecker()
     {
-        
-        RetractHook();
+        if (clawLeftCurrentDistance >= maxDistance)
+        {
+            StartCoroutine(RetractHook(clawLeft, clawLeftInitialPosition));
+        }
+
+        if (clawRightCurrentDistance >= maxDistance)
+        {
+            StartCoroutine(RetractHook(clawRight, clawRightInitialPosition));
+        }
     }
 }
