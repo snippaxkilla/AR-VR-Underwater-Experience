a1f50aa6b55e765b6a4b086f2aa7600c08967360
snippaxkilla <43402335+snippaxkilla@users.noreply.github.com>
2023-04-09 15:52:05 +0200

    uses fixed joint instead of parenting to claw

--- Assets/Scripts/Claw.cs
+++ Assets/Scripts/Claw.cs
@@ -6,57 +6,53 @@ public class Claw : MonoBehaviour
     [SerializeField] private GrapplingHook GrapplingHookGun;
 
     // Ref values we need for retracting the claw
-    [SerializeField] private Vector3 leftRetractOrigin;
-    [SerializeField] private Vector3 rightRetractOrigin;
-    private GrapplingHook.ClawState leftState;
-    private GrapplingHook.ClawState rightState;
+    private Vector3 leftRetractOrigin;
+    private Vector3 rightRetractOrigin;
+    private GrapplingHook.ClawState clawState;
 
     private bool isHooked;
     private GameObject hookedGarbage;
 
     private void OnTriggerEnter(Collider other)
     {
-        hookedGarbage = other.gameObject;
-        isHooked = true;
+        if (other.transform.parent != null && other.transform.parent.gameObject == GroupedGarbage)
+        {
+            hookedGarbage = other.gameObject;
+            isHooked = true;
 
-        if (transform.name == "ClawLeft")
-        { 
-            GrapplingHookGun.RetractClaw(GrapplingHookGun.GetLeftRigidbody(), ref leftState, ref leftRetractOrigin);
-        }
-        if (transform.name == "ClawRight")
-        { 
-            GrapplingHookGun.RetractClaw(GrapplingHookGun.GetRightRigidbody(), ref rightState, ref rightRetractOrigin);
+            FixedJoint fixedJoint = CreateFixedJoint(gameObject, hookedGarbage);
+
+            if (transform.name == "ClawLeft")
+            {
+                GrapplingHookGun.RetractClaw(GrapplingHookGun.GetLeftRigidbody(), ref clawState, ref leftRetractOrigin);
+            }
+            if (transform.name == "ClawRight")
+            {
+                GrapplingHookGun.RetractClaw(GrapplingHookGun.GetRightRigidbody(), ref clawState, ref rightRetractOrigin);
+            }
         }
     }
 
     private void Update()
     {
         GarbageDestroyer();
-
-        if (isHooked)
-        {
-            UpdateGarbagePosition(hookedGarbage);
-        }
     }
 
-    private void UpdateGarbagePosition(GameObject objectToPull)
-    {
-        Transform clawTransform = transform;
-        objectToPull.transform.position = clawTransform.position;
-    }
-
+    // Destroy the garbage when the claw is retracted so the claw state is idle
     private void GarbageDestroyer()
     {
-        if (GrapplingHookGun.GetLeftState() == GrapplingHook.ClawState.Idle && isHooked)
+        if (clawState == GrapplingHook.ClawState.Idle && isHooked)
         {
+            Destroy(GetComponent<FixedJoint>());
             Destroy(hookedGarbage);
             isHooked = false;
         }
+    }
 
-        if (GrapplingHookGun.GetRightState() == GrapplingHook.ClawState.Idle && isHooked)
-        {
-            Destroy(hookedGarbage);
-            isHooked = false;
-        }
+    private FixedJoint CreateFixedJoint(GameObject claw, GameObject targetObject)
+    {
+        FixedJoint fixedJoint = claw.AddComponent<FixedJoint>();
+        fixedJoint.connectedBody = targetObject.GetComponent<Rigidbody>();
+        return fixedJoint;
     }
 }
