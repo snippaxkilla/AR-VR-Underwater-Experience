a74269f81bbae2c7a58b6eebe861dbd8b6ad6133
ThaGlenniator <glenn.wustlich@gmail.com>
2023-05-02 18:51:14 +0200

    start wiggle script

--- Assets/testScript.cs
+++ Assets/testScript.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class testScript : MonoBehaviour
 {
+    public InterpolateFog fogscript;
     // Start is called before the first frame update
     void Start()
     {
-        
+        fogscript.fogChange(InterpolateFog.FogStates.s100);
     }
 
     // Update is called once per frame
