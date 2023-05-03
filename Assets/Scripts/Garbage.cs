f3e59f01a36b62c53ede71cc5e747f5a9bd625b6
snippaxkilla <43402335+snippaxkilla@users.noreply.github.com>
2023-05-03 12:09:14 +0200

    all garbage now has rigidbodies
    also made a start with the garbage being thrown down effect.

--- Assets/Scripts/Garbage.cs
+++ Assets/Scripts/Garbage.cs
@@ -9,9 +9,23 @@ public class Garbage : MonoBehaviour
         Large
     }
 
+    public enum GarbageState
+    {
+        Floating,
+        Retracting,
+        Drowning
+    }
+
+    [Header("Garbage size doesn't influence the retract speed it groups it into spawning types")]
     [SerializeField] private GarbageSize size;
+    [Header("The amount of points will change the environment")]
     [SerializeField] private int points;
-    [SerializeField] private float retractSpeed = 1;
+    [Header("How fast will my garbage retract by my grappling hook")]
+    [SerializeField] private float retractSpeed = 1f;
+    [Header("How fast will my garbage drown")]
+    [SerializeField] private float drowningSpeed = 1f;
+    [Header("The time it will take to transition back to floating state")]
+    [SerializeField] private float drowningTime = 2f;
 
     public GarbageSize GetSize()
     {
